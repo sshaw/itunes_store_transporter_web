233f3e3a855193c561c719ed132e3aa4f7c8c556
sshaw <skye.shaw@gmail.com>
2015-12-20 16:03:19 -0500

    Feature specs for account configuration

--- spec/features/configuration_spec.rb
+++ spec/features/configuration_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 feature "Configuration", :js do
   before do
-    # Avoid account setup page
-    create(:account)
+    # Creating an account also avoids redirects to account setup page
+    @account = create(:account)
     visit app.url(:config)
   end
 
@@ -22,5 +22,63 @@ feature "Configuration", :js do
     expect(page).to have_css("span[data-content='#{Dir.tmpdir}']")
     expect(page).to have_field("Rate", :with => "999")
     expect(page).to have_field("Transport", :with => "Aspera")
+  end
+
+  scenario "Creating an account" do
+    click_on "Accounts"
+    click_on "New Account"
+
+    expect(page).to have_text("New Account")
+
+    fill_in "Username", :with => "sshaw"
+    fill_in "Password", :with => "what_it_iz!@#"
+    fill_in "Shortname", :with => "galinha"
+
+    click_on "Create"
+
+    expect(page).to have_text("Account created")
+
+    click_on "Account"
+
+    expect(page).to have_text("sshaw")
+    expect(page).to have_text("galinha")
+    expect(page).to_not have_text("what_it_iz!@#")
+  end
+
+  scenario "Editing an account" do
+    click_on "Accounts"
+    within_account @account do
+      click_on "Edit"
+    end
+
+    fill_in "Username", :with => @account.username << "X"
+    fill_in "Password", :with => "foo"
+    fill_in "Shortname", :with => @account.shortname << "X"
+    click_on "Update"
+
+    expect(page).to have_text("Account updated")
+
+    click_on "Account"
+
+    within_account @account do
+      expect(page).to have_text(@account.username)
+      expect(page).to have_text(@account.shortname)
+      expect(page).to_not have_text(@account.password)
+    end
+  end
+
+  scenario "Deleting an account" do
+    click_on "Accounts"
+    within_account @account do
+      click_on "Delete"
+    end
+
+    expect(page).to_not have_css("#account_#{@account.id}")
+  end
+
+  def within_account(account)
+    within "#account_#{account.id}" do
+      yield
+    end
   end
 end
