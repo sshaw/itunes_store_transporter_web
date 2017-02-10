e8acde7e60621f71b2def84400263fdc0c2bbdc0
sshaw <skye.shaw@gmail.com>
2017-02-09 23:47:42 -0500

    Update itmsworker.bat with notifications task [ci skip]

--- bin/itmsworker.bat
+++ bin/itmsworker.bat
@@ -9,11 +9,18 @@ set BUNDLE_GEMFILE=%root%..\Gemfile.%platform%
 set task=%1
 
 if exist "%BUNDLE_GEMFILE%" (
-  if "%task%" == "" set task=work
+   set valid=0
+
+   if "%task%" == "jobs" set valid=1
+   if "%task%" == "notifications" set valid=1
+   if not "%valid%" == "1" (
+     echo usage: itmsworker [jobs^|notifications]
+     exit /b 1
+  )
+
   ruby "%root%padrino" rake -c "%root%.." -e production jobs:!task!
 ) else (
   echo Missing file %BUNDLE_GEMFILE%
   echo You must run: ruby setup.rb
   exit /b 1
 )
-
