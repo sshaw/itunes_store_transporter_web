46346122348367f31a5d8b13dab4850ee2b8a06d
sshaw <skye.shaw@gmail.com>
2017-03-16 23:28:54 -0400

    Default to jobs tasks in itmsweb.bat

--- bin/itmsworker.bat
+++ bin/itmsworker.bat
@@ -8,14 +8,15 @@ set root=%~dp0
 set BUNDLE_GEMFILE=%root%..\Gemfile.%platform%
 set task=%1
 
+if "%task%" == "" set task=jobs
+
 if exist "%BUNDLE_GEMFILE%" (
    set valid=0
-   REM TODO: default to jobs!
    if "%task%" == "jobs" set valid=1
    if "%task%" == "notifications" set valid=1
    if "%task%" == "hooks" set valid=1
    if not "%valid%" == "1" (
-     echo usage: itmsworker [jobs^|notifications]
+     echo usage: itmsworker [jobs^|hooks^|notifications]
      exit /b 1
   )
 
