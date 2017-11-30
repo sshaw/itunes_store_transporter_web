2fadf5beb2c71e2f9ec3e8befd5d681843e8e529
sshaw <skye.shaw@gmail.com>
2017-11-30 00:09:03 -0800

    Add command line arguments to install script

--- install.rb
+++ install.rb
@@ -1,6 +1,7 @@
 require "erb"
 require "fileutils"
 require "yaml"
+require "optparse"
 
 abort "Ruby >= 1.9 is required, you're running #{RUBY_VERSION}" if RUBY_VERSION < "1.9"
 
@@ -20,65 +21,112 @@ DBDependency = Struct.new(:name, :gem, :adapter, :version) do
   end
 end
 
-def config_database
-  config  = { "name" => DBNAME }
-  drivers = [
-    DBDependency.new("MySQL", "mysql2", nil, "~> 0.3.18"),
-    DBDependency.new("PostgreSQL", "postgresql", "pg"),
-    DBDependency.new("SQLite", "sqlite3")
-  ]
+$config = {
+  :db_name => DBNAME,
+  :prompt => true
+}
 
-  puts "Select your database: "
-  drivers.each_with_index { |dep, i| puts "  #{i + 1}: #{dep.name}" }
-  i = gets
-  unless i =~ /\A\d+\Z/ && (pos = i.to_i - 1) >= 0 && pos < drivers.size
-    abort "Unknown choice '#{i.chomp}'"
+$supported_drivers = [
+  DBDependency.new("MySQL", "mysql2", nil, "~> 0.3.18"),
+  DBDependency.new("PostgreSQL", "postgresql", "pg"),
+  DBDependency.new("SQLite", "sqlite3")
+]
+
+$default_driver = $supported_drivers[2]
+
+parser = OptionParser.new do |opts|
+  opts.banner = "usage: #{File.basename($0)} [--db-driver=name] [--db-user=name] [--db-password=name] [--db-host=name] [--no-prompt]"
+
+  opts.on "-h", "--help", "Show this message" do
+    puts opts
+    exit
   end
 
-  config["adapter"] = drivers[pos].adapter
-  config["gem"] = drivers[pos].to_gem
+  opts.on "--[no-]prompt", "Prompt or don't prompt user for config options" do |prompt|
+    $config[:prompt] = prompt
+  end
 
-  if config["adapter"] == "sqlite3"
+  opts.on "--db-driver=NAME", $supported_drivers.map { |d| d.name.downcase }, "DB driver" do |name|
+    $config[:db_driver] = $supported_drivers.find { |d| d.name.downcase == name }
+  end
+
+  opts.on "--db-password=NAME", "DB password" do |name|
+    $config[:db_password] = name
+  end
+
+  opts.on "--db-host=NAME", "DB hostname" do |name|
+    $config[:db_host] = name
+  end
+
+  opts.on "--db-user=NAME", "DB username" do |name|
+    $config[:db_user] = name
+  end
+end.parse!
+
+def prompt_for_config
+  unless $config[:db_driver]
+    puts "Select your database: "
+    $supported_drivers.each_with_index { |dep, i| puts "#{i + 1}: #{dep.name}" }
+
+    i = gets
+    exit unless i # EOF
+    abort "Unknown choice '#{i.chomp}'" unless i =~ /\A\d+\Z/ && $supported_drivers[pos = i.to_i]
+  end
+
+  unless $config[:db_host]
+    print "Database host [localhost]: "
+    host = gets
+    exit unless host
+
+    $config[:db_host] = host.chomp if host =~ /\w/
+  end
+
+  %w[username password].each do |opt|
+    key = :"db_#{opt}"
+    next if $config[key]
+
+    print "Database #{opt}: "
+    choice = gets
+    exit unless choice
+
+    $config[key] = choice.chomp
+  end
+end
+
+def install
+  puts "Installing..."
+
+  db_driver = $config[:db_driver] || $default_driver
+  db_config = {
+    "name" => $config[:db_name],
+    "adapter" => db_driver.adapter,
+    "host" => $config[:db_host] || "localhost",
+    "username" => $config[:db_user],
+    "password" => $config[:db_password],
+  }
+
+  if db_config[:adapter] == "sqlite3"
     path = "#{ROOT}/db"
     Dir.mkdir(path) unless File.directory?(path)
-    config["name"] = "#{path}/#{DBNAME}.sqlite3"
-    config["timeout"] = 5000
-    return config
+    db_config["name"] = "#{path}/#{$config[:db_name]}}.sqlite3"
+    db_config["timeout"] = 5000
   end
 
-  print "Host [localhost]: "
-  config["host"] = gets.chomp
-  config["host"] = "localhost" unless config[:host] =~ /\w/
-  %w[username password].each do |opt|
-    print "#{opt.to_s.capitalize}: "
-    config[opt] = gets.chomp
-  end
-
-  config
-end
-
-def write_config(config)
   File.open("#{ROOT}/config/itmsweb.yml", "w") do |io|
-    io.puts YAML.dump("database" => config)
+    io.puts YAML.dump("database" => db_config)
   end
-end
 
-def install(config)
   gemfile = "#{ROOT}/Gemfile.#{RUBY_PLATFORM}"
   FileUtils.cp("#{ROOT}/Gemfile", gemfile)
-  File.open(gemfile, "a") { |io| io.puts config["gem"] }
-
-  config.delete("gem")
-  write_config(config)
+  File.open(gemfile, "a") { |io| io.puts db_driver.to_gem }
 
   ENV["BUNDLE_GEMFILE"] = gemfile
 
-  puts "Installing dependencies"
   # TODO: update these in 0.2.0 to use new Bundler options
   commands = ["bundle install --path vendor/bundle --without=test development --binstubs",
               "#{RAKE} ar:setup"]
   begin
-    # Just look up gem!
+    # Just look up gem?
     Gem.bin_path "bundler", "bundle"
   rescue Gem::GemNotFoundException => e
     commands.unshift "gem install bundler"
@@ -94,14 +142,12 @@ def install(config)
     %w[padrino itmsweb itmsworker].include?(File.basename(path).sub(/\.\w+\Z/, ""))
   })
 
+  # iTMSTransporter logs go here by default
   FileUtils.mkdir_p("#{ROOT}/var/lib/output")
 end
 
-print "Internet access required, is this OK? [y/N]: "
-opt = gets
-exit unless opt =~ /\Ay/i
-
-install(config_database)
+prompt_for_config if $config[:prompt]
+install
 
 puts(<<MSG)
 Installation successful!
@@ -114,4 +160,7 @@ Please report any issues at https://github.com/sshaw/itunes_store_transporter_we
 
 Thanks for using the iTunes Store Transporter: GUI.
 http://transportergui.com
+
+Made by ScreenStaring
+http://ScreenStaring.com
 MSG
