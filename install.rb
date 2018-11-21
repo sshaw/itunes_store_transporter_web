59b3b2c4295f02938e2882482a6aaf530086aaab
sshaw <skye.shaw@gmail.com>
2018-11-21 15:56:08 -0500

    Fix db username typo in install.rb

--- install.rb
+++ install.rb
@@ -108,7 +108,7 @@ def install
     "name" => $config[:db_name],
     "adapter" => db_driver.adapter,
     "host" => $config[:db_host] || "localhost",
-    "username" => $config[:db_usermae],
+    "username" => $config[:db_username],
     "password" => $config[:db_password],
   }
 
